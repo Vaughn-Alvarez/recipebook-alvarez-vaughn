5ca4caed4c9d51f74fd91ece9c261987cfe0946f
Vaughn Alvarez <vaughn.alvarez@obf.ateneo.edu>
2024-03-03 22:21:06 +0800

    changed views to classes

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,31 +1,39 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Recipe(models.Model):
 
     def __str__(self) -> str:
         return self.name
-    
+
+    def get_absolute_url(self):
+        return reverse('ledger:recipe', args=[self.pk])
+
     name = models.CharField(max_length=100)
-   
+
+
 class Ingredient(models.Model):
+
+    name = models.CharField(max_length=100)
 
     def __str__(self) -> str:
         return self.name
-    
-    name = models.CharField(max_length=100)
+
+    def get_absolute_url(self):
+        return reverse('ledger:recipe', args=[self.pk])
+
 
 class RecipeIngredient(models.Model):
-    
+
     quantity = models.CharField(max_length=100)
     recipe = models.ForeignKey(
-                "Recipe",
-                on_delete=models.CASCADE,
-                related_name="ingredient"
-            )
+        "Recipe",
+        on_delete=models.CASCADE,
+        related_name="ingredient"
+    )
     ingredient = models.ForeignKey(
-                "Ingredient",
-                on_delete=models.CASCADE,
-                related_name="recipe"
-            )
-    
-    
+        "Ingredient",
+        on_delete=models.CASCADE,
+        related_name="recipe"
+    )

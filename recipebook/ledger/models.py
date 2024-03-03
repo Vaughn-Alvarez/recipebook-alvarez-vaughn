cb8543d9f09a795b34ace416a20969d7109c6a09
Vaughn Alvarez <vaughn.alvarez@obf.ateneo.edu>
2024-03-03 18:44:50 +0800

    added models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+
+    def __str__(self) -> str:
+        return self.name
+    
+    name = models.CharField(max_length=100)
+   
+class Ingredient(models.Model):
+
+    def __str__(self) -> str:
+        return self.name
+    
+    name = models.CharField(max_length=100)
+
+class RecipeIngredient(models.Model):
+    
+    quantity = models.CharField(max_length=100)
+    recipe = models.ForeignKey(
+                "Recipe",
+                on_delete=models.CASCADE,
+                related_name="ingredient"
+            )
+    ingredient = models.ForeignKey(
+                "Ingredient",
+                on_delete=models.CASCADE,
+                related_name="recipe"
+            )
+    
+    
